15ce33e3032b32b2d7244cd627fa254495eabeb7
Jonas Hagmar <jonas.hagmar@gmail.com>
2013-02-04 16:12:55 +0100

    added GPL headers

--- libcx9r/aes256.c
+++ libcx9r/aes256.c
@@ -1,3 +1,22 @@
+/* Cryptkeyper is
+ *
+ *     Copyright (C) 2013 Jonas Hagmar (jonas.hagmar@gmail.com)
+ *
+ * This file is part of Cryptkeyper.
+ *
+ * Cryptkeyper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * Cryptkeyper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cryptkeyper. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "aes256.h"
 
 void aes256_ecb_init(aes256_ecb_ctx *ctx, uint8_t *key)
