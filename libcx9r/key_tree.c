38fed86c8c4db21cd565033935ffadd5af073155
Jonas Hagmar <jonas.hagmar@gmail.com>
2013-02-24 21:34:32 +0100

    more key tree implementation and test

--- libcx9r/key_tree.c
+++ libcx9r/key_tree.c
@@ -1,12 +1,25 @@
-/*
- * key_tree.c
+/* Cryptkeyper is
  *
- *  Created on: 22 feb 2013
- *      Author: jhagmar
+ *     Copyright (C) 2013 Jonas Hagmar (jonas.hagmar@gmail.com)
+ *
+ * This file is part of cryptkeyper.
+ *
+ * Cryptkeyper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * Cryptkeyper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cryptkeyper. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "key_tree.h"
 #include <stdlib.h>
+#include <string.h>
 
 cx9r_key_tree *cx9r_key_tree_create() {
 	cx9r_key_tree *kt;
@@ -18,11 +31,46 @@ cx9r_key_tree *cx9r_key_tree_create() {
 	kt->root.children = NULL;
 	kt->root.next = NULL;
 	kt->root.entries = NULL;
+	kt->root.name = NULL;
 
 	return kt;
 }
 
-cx9r_kt_group *cx9r_key_tree_get_root(cx9r_key_tree const *kt) {
+static void field_free(cx9r_kt_field *ktf) {
+	if (ktf != NULL) {
+		field_free(ktf->next);
+		free(ktf->name);
+		free(ktf->value);
+		free(ktf);
+	}
+}
+
+static void entry_free(cx9r_kt_entry *kte) {
+	if (kte != NULL) {
+		entry_free(kte->next);
+		field_free(kte->fields);
+		free(kte->name);
+		free(kte);
+	}
+}
+
+static void group_free(cx9r_kt_group *ktg) {
+	if (ktg != NULL) {
+		group_free(ktg->children);
+		group_free(ktg->next);
+		entry_free(ktg->entries);
+		free(ktg->name);
+		free(ktg);
+	}
+}
+
+void cx9r_key_tree_free(cx9r_key_tree *kt) {
+	group_free(kt->root.children);
+	entry_free(kt->root.entries);
+	free(kt->root.name);
+}
+
+cx9r_kt_group *cx9r_key_tree_get_root(cx9r_key_tree *kt) {
 	return &kt->root;
 }
 
@@ -36,6 +84,26 @@ cx9r_kt_group *cx9r_kt_group_get_next(cx9r_kt_group const *ktg) {
 
 cx9r_kt_entry *cx9r_kt_group_get_entries(cx9r_kt_group const *ktg) {
 	return ktg->entries;
+}
+
+char const *cx9r_kt_group_get_name(cx9r_kt_group const *ktg) {
+	return ktg->name;
+}
+
+char const *cx9r_kt_group_set_name(cx9r_kt_group *ktg, char const *name) {
+	size_t length;
+
+	length = strlen(name) + 1;
+	if (ktg->name != NULL) {
+		free(ktg->name);
+		ktg->name = NULL;
+	}
+	ktg->name = malloc(length);
+	if (ktg->name == NULL) {
+		return NULL;
+	}
+	strcpy(ktg->name, name);
+	return ktg->name;
 }
 
 cx9r_kt_group *cx9r_kt_group_add_child(cx9r_kt_group *ktg) {
@@ -56,9 +124,134 @@ cx9r_kt_group *cx9r_kt_group_add_child(cx9r_kt_group *ktg) {
 		return NULL;
 	}
 
+	c->name = NULL;
 	c->children = NULL;
 	c->entries = NULL;
 	c->next = NULL;
+	c->name = NULL;
 	*slot = c;
 	return c;
+}
+
+cx9r_kt_entry *cx9r_kt_group_add_entry(cx9r_kt_group *ktg) {
+	cx9r_kt_entry *e;
+	cx9r_kt_entry **slot;
+
+	//find the slot to fill with the newly allocated entry
+	if (ktg->entries == NULL) {
+		slot = &ktg->entries;
+	}
+	else {
+		e = ktg->entries;
+		while (e->next != NULL) e = e->next;
+		slot = &e->next;
+	}
+
+	if ((e = malloc(sizeof(cx9r_kt_entry))) == NULL) {
+		return NULL;
+	}
+
+	e->next = NULL;
+	e->fields = NULL;
+	e->name = NULL;
+	*slot = e;
+	return e;
+}
+
+char const *cx9r_kt_entry_get_name(cx9r_kt_entry *kte) {
+	return kte->name;
+}
+
+char const *cx9r_kt_entry_set_name(cx9r_kt_entry *kte, char const *name) {
+	size_t length;
+
+	length = strlen(name) + 1;
+	if (kte->name != NULL) {
+		free(kte->name);
+		kte->name = NULL;
+	}
+	kte->name = malloc(length);
+	if (kte->name == NULL) {
+		return NULL;
+	}
+	strcpy(kte->name, name);
+	return kte->name;
+}
+
+cx9r_kt_field *cx9r_kt_entry_get_fields(cx9r_kt_entry *kte) {
+	return kte->fields;
+}
+
+cx9r_kt_field *cx9r_kt_entry_add_field(cx9r_kt_entry *kte) {
+	cx9r_kt_field *f;
+	cx9r_kt_field **slot;
+
+	//find the slot to fill with the newly allocated entry
+	if (kte->fields == NULL) {
+		slot = &kte->fields;
+	} else {
+		f = kte->fields;
+		while (f->next != NULL)
+			f = f->next;
+		slot = &f->next;
+	}
+
+	if ((f = malloc(sizeof(cx9r_kt_field))) == NULL) {
+		return NULL;
+	}
+
+	f->name = NULL;
+	f->next = NULL;
+	f->value = NULL;
+	*slot = f;
+	return f;
+}
+
+cx9r_kt_entry *cx9r_kt_entry_get_next(cx9r_kt_entry *kte) {
+	return kte->next;
+}
+
+char const *cx9r_kt_field_get_name(cx9r_kt_field *ktf) {
+	return ktf->name;
+}
+
+char const *cx9r_kt_field_set_name(cx9r_kt_field *ktf, char const *name) {
+	size_t length;
+
+	length = strlen(name) + 1;
+	if (ktf->name != NULL) {
+		free(ktf->name);
+		ktf->name = NULL;
+	}
+	ktf->name = malloc(length);
+	if (ktf->name == NULL) {
+		return NULL;
+	}
+	strcpy(ktf->name, name);
+	return ktf->name;
+}
+
+char const *cx9r_kt_field_get_value(cx9r_kt_field *ktf) {
+	return ktf->value;
+}
+
+char const *cx9r_kt_field_set_value(cx9r_kt_field *ktf, char const *value) {
+	size_t length;
+
+	length = strlen(value) + 1;
+	if (ktf->value != NULL) {
+		free(ktf->value);
+		ktf->value = NULL;
+	}
+	ktf->value = malloc(length);
+	if (ktf->value == NULL) {
+		return NULL;
+	}
+	strcpy(ktf->value, value);
+	return ktf->value;
+
+}
+
+cx9r_kt_field *cx9r_kt_field_get_next(cx9r_kt_field *ktf) {
+	return ktf->next;
 }
--- libcx9r/key_tree.h
+++ libcx9r/key_tree.h
@@ -20,38 +20,57 @@
 #ifndef CX9R_KEY_TREE_H
 #define CX9R_KEY_TREE_H
 
-typedef struct {
+typedef struct cx9r_ktf cx9r_kt_field;
+
+struct cx9r_ktf {
 	char *name;
 	char *value;
-} cx9r_kt_field;
+	cx9r_kt_field *next;
+};
 
-typedef struct cx9r_kt_entry;
+typedef struct cx9r_kte cx9r_kt_entry;
 
-typedef struct {
-	char *key;
+struct cx9r_kte {
+	char *name;
 	cx9r_kt_field *fields;
 	cx9r_kt_entry *next;
-} cx9r_kt_entry;
+};
 
-typedef struct cx9r_kt_group;
+typedef struct cx9r_ktg cx9r_kt_group;
 
-typedef struct {
+struct cx9r_ktg {
+	char *name;
 	cx9r_kt_group *children;
 	cx9r_kt_group *next;
 	cx9r_kt_entry *entries;
-} cx9r_kt_group;
+};
 
 typedef struct {
 	cx9r_kt_group root;
 } cx9r_key_tree;
 
 cx9r_key_tree *cx9r_key_tree_create();
-cx9r_kt_group *cx9r_key_tree_get_root(cx9r_key_tree const *kt);
+cx9r_kt_group *cx9r_key_tree_get_root(cx9r_key_tree *kt);
+void cx9r_key_tree_free(cx9r_key_tree *kt);
 
 cx9r_kt_group *cx9r_kt_group_get_children(cx9r_kt_group const *ktg);
 cx9r_kt_group *cx9r_kt_group_get_next(cx9r_kt_group const *ktg);
 cx9r_kt_entry *cx9r_kt_group_get_entries(cx9r_kt_group const *ktg);
+char const *cx9r_kt_group_get_name(cx9r_kt_group const *ktg);
+char const *cx9r_kt_group_set_name(cx9r_kt_group *ktg, char const *name);
 cx9r_kt_group *cx9r_kt_group_add_child(cx9r_kt_group *ktg);
+cx9r_kt_entry *cx9r_kt_group_add_entry(cx9r_kt_group *ktg);
 
+char const *cx9r_kt_entry_get_name(cx9r_kt_entry *kte);
+char const *cx9r_kt_entry_set_name(cx9r_kt_entry *kte, char const *name);
+cx9r_kt_field *cx9r_kt_entry_get_fields(cx9r_kt_entry *kte);
+cx9r_kt_field *cx9r_kt_entry_add_field(cx9r_kt_entry *kte);
+cx9r_kt_entry *cx9r_kt_entry_get_next(cx9r_kt_entry *kte);
+
+char const *cx9r_kt_field_get_name(cx9r_kt_field *ktf);
+char const *cx9r_kt_field_set_name(cx9r_kt_field *ktf, char const *name);
+char const *cx9r_kt_field_get_value(cx9r_kt_field *ktf);
+char const *cx9r_kt_field_set_value(cx9r_kt_field *ktf, char const *value);
+cx9r_kt_field *cx9r_kt_field_get_next(cx9r_kt_field *ktf);
 
 #endif
